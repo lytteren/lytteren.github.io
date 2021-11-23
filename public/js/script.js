ebac8536fa5738a18cf2a4a153efaca46ad27cce
lytsites <lyttern.lu@gmail.com>
2021-11-23 23:15:44 +0600

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,151 +1,44 @@
-function generation () {
+function getRandom(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 
-	$('.game-box span').text('');
-
-	// Получение рандомного числа
-
-	function randomInteger(min, max) {
-		let rand = min - 0.5 + Math.random() * (max - min + 1);
-		return Math.round(rand);
-	}
-
-	// Выборка рандомных элементов
-
-	root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-	square = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400];
-	array = [];
-
-	for (var i = 0 ; (i < $('.game-row').length) && (i < square.length) ; i++) {
-		var r = Math.floor(Math.random() * (square.length - i)) + i;
-		var number = square[r];
-		square[r] = square[i];
-		square[i] = number;
-
-		array.push(number);
-	}
-
-	// Расстановка элементов
-
-	for (i = 1; i <= array.length; i++) {
-		item = randomInteger(1, $('.row-1 .game-box').length);
-		$('.row-' + i + ' .box-' + item + ' span').text(array[i - 1]);
-	}
-
-	// Выбор главное числа
-
-	number = Math.sqrt(array[randomInteger(0, array.length - 1)]);
-	$('.main-number span').text(number);
+function getQuestion () {
+	$('li').removeClass('active');
+	questions = new Array();
+	$.get('/public/js/question.txt', function(data) {
+		questions = data.split('\n');
+		el = questions[getRandom(0, questions.length - 1)].split('~');
+		question = el[0];
+		answers = el.slice(1);
+		$('.b-question h3').text(question.replaceAll('"', ''));
+		answers = answers.sort(function(){
+			return Math.random() - 0.5;
+		});
+		for (i = 0; i < answers.length; i++) {
+			if (answers[i].indexOf('<>') !== -1) {
+				$('.b-answers .li-' + i).addClass('active');
+			}
+			$('.b-answers .li-' + i).text(answers[i].replaceAll('"', '').replace('<>', ''));
+		}
+	});
 }
 
 $(document).ready(function () {
 
-	$('body').css({
-		'visibility': 'visible',
-	})
-
-	bool = false;
-
-	for (i = 1; i <= 8; i++) {
-		$('.game-area').append($('<div>', {
-			'class': 'game-row row-' + i
+	respons = prompt('Пароль:');
+	if (respons == 12345) {
+		$('body').css({
+			'display': 'block',
 		})
-		.append(function () {
-			for (j = 1; j <= 10; j++) {
-				$(this).append($('<div>', {
-					'class': 'game-box box-' + j
-				})
-				.append($('<span>', {
-
-				})))
-			}
-		}))
+		getQuestion();
+		$('body').on('click', '.b-answers li.active', function () {
+			getQuestion();
+		})
 	}
-
-	aHeight = $(window).outerHeight();
-	aWidth = aHeight;
-
-	$('.side-options').css({
-		'width': ($(window).outerWidth() - aWidth) / 2 - 50,
-	})
-
-	$('.game-area').css({
-		'width': aWidth,
-		'height': aHeight,
-	});
-
-	$('.game-options').css({
-		'height': aWidth / 5,
-	})
-
-	bHeight = aHeight / 10;
-	bWidth = bHeight;
-
-	$('.game-box').css({
-		'width': bWidth,
-		'height': bHeight,
-	});
-
-	// Проверка ответа
-
-	score = 0;
-
-	$('.game-row .game-box').on('click', function () {
-		if (bool) {
-			number = $(this, 'span').text();
-			mainNumber = $('.main-number span').text();
-			if (Math.sqrt(number) == mainNumber) {
-				score += 1;
-				if (score < 10 && score >= 0) {
-					$('.game-score span').text('0' + score);
-				}
-				else {
-					$('.game-score span').text(score);
-				}
-				generation();
-			}
-			else {
-				score -= 1;
-				if (score < 10 && score >= 0) {
-					$('.game-score span').text('0' + score);
-				}
-				else {
-					$('.game-score span').text(score);
-				}
-				generation();
-			}
-		}
-	})
-
-	$('.start-game').on('click', function () {
-		bool = true;
-		generation();
-
-		timeMinut = 30;
-
-		timer = setInterval(function () {
-			seconds = timeMinut%60
-			minutes = timeMinut/60%60
-			hour = timeMinut/60/60%60
-			if (timeMinut <= 0) {
-				clearInterval(timer);
-				$('.game-score span').text('00')
-				$('.game-box span').text('');
-				$('.game-time span').text('00');
-
-				$('.result span').text(score)
-
-			} else {
-				let strTimer = `${seconds}`;
-				if (strTimer < 10) {
-					$('.game-time span').text('0' + strTimer);
-				}
-				else {
-					$('.game-time span').text(strTimer);
-				}
-			}
-			--timeMinut;
-		}, 1000)
-
-	})
+	else {
+		window.location.reload();
+	}
 
 })

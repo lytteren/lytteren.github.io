a01ccb7e832b7a600e45dc287d42ecc78418a87f
lytsites <lyttern.lu@gmail.com>
2021-11-24 21:35:25 +0600

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,74 +1,91 @@
-function getRandom(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-
-function nextQuestion (count, questions, correct) {
-	try {
-		el = questions[count].split('~');
-		question = el[0];
-		answers = el.slice(1);
-		$('.b-question h3').text(question.replaceAll('"', ''));
-		answers = answers.sort(() => 0.5 - Math.random());
-		for (i = 0; i < answers.length; i++) {
-			if (answers[i].indexOf('<>') !== -1) {
-				$('.b-answers .li-' + i).addClass('active on');
-			}
-			$('.b-answers .li-' + i).text(answers[i].replaceAll('"', '').replace('<>', ''));
-		}
-	}
-	catch (err) {
-		console.log('End');
-		// alert('Правильных: ' + correct)
-	}
-}
-
 $(document).ready(function () {
 
-	// respons = prompt('Пароль:');
-	// if (respons == '0420zxc0420') {
-		$('body').css({
+	$('body').css({
+		'display': 'block',
+	})
+
+	$('li').removeClass('active');
+
+	count = 1;
+	correct = 0;
+	questions_obj = {};
+
+	$.get('/public/js/question.txt', function(data) {
+		all = data.split('\n');
+		questions = all.sort(() => 0.5 - Math.random()).slice(0, 25);
+		for (i = 1; i <= questions.length; i++) {
+			el = questions[i - 1].split('~');
+			// console.log(el);
+			// console.log(el[5]);
+			$('.blocks').append($('<div>', {
+				'class': 'block block-' + i,
+				'text': el[0],
+			})
+			.prepend($('<pre>', {
+				'text': 'Вопрос: ' + i
+			}))
+			.append($('<div>', {
+				'class': 'b-question',
+			})
+			.append($('<h3>')))
+			.append($('<ul>', {
+				'class': 'b-answers',
+			})
+			.append(function () {
+				html = '';
+				answers = el.slice(1);
+				answers = answers.sort(() => 0.5 - Math.random());
+				for (j = 0; j < 5; j++) {
+					if (answers[j].indexOf('<>') !== -1) {
+						html += '<li class="li-' + j + 1 + ' active">' + answers[j].replaceAll('"', '').replace('<>', '') + '</li>'
+					}
+					else {
+						html += '<li class="li-' + j + 1 + '">' + answers[j].replaceAll('"', '').replace('<>', '') + '</li>'
+					}
+				}
+				return html;
+			})))
+		}
+	});
+
+	$('body').on('click', '.b-answers li', function () {
+		if (count < 25) {
+			count += 1;
+		}
+
+		$('.pre-1').text('Вопрос: ' + count);
+		$(this).closest('.block').css({
+			'display': 'none',
+		})
+		$('.block-' + count).css({
 			'display': 'block',
 		})
 
-		$('li').removeClass('active');
+		if ($(this).hasClass('active')) {
+			$(this).addClass('true');
+			correct += 1;
+			$('.pre-2').text('Правильных: ' + correct);
+		}
+		else {
+			$(this).addClass('false');
+		}
+	})
 
-		count = 1;
-		correct = 0;
-
-		$.get('/public/js/question.txt', function(data) {
-			questions = data.split('\n');
-			shuffled_questions = questions.sort(() => 0.5 - Math.random());
-			selected_questions = shuffled_questions.slice(0, 26);
-			nextQuestion(count, selected_questions);
-			$('body').on('click', '.b-answers li', function () {
-				if ($(this).hasClass('active')) {
-					correct += 1;
-					$('.code-2').text('Правильных: ' + correct);
-				}
-				$('li').removeClass('active');
-				if (count !== 25) {
-					count += 1;
-					nextQuestion(count, selected_questions, correct);
-				}
-				$('.code-1').text('Вопрос: ' + count);
-			})
-		});
+	$('body').on('click', '.block-25 li', function () {
+		$('.block').css({
+			'display': 'block',
+			'pointer-events': 'none',
+		})
+		$('li.active').addClass('true')
+	})
 
 		setInterval(function () {
-			if ($('input').prop('checked')) {
-				$('li.active').addClass('on')
-			}
-			else {
-				$('li.active').removeClass('on')
-			}
-		})
-
-
-	// }
-	// else {
-	// 	window.location.reload();
-	// }
+		if ($('input').prop('checked')) {
+			$('li.active').addClass('on')
+		}
+		else {
+			$('li.active').removeClass('on')
+		}
+	})
 
 })

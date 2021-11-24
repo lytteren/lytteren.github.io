7fd67abf48e19e25122a07f3142e8a8f1091ee38
lytsites <lyttern.lu@gmail.com>
2021-11-24 15:49:08 +0600

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -4,25 +4,24 @@ function getRandom(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-function getQuestion () {
-	$('li').removeClass('active');
-	questions = new Array();
-	$.get('/public/js/question.txt', function(data) {
-		questions = data.split('\n');
-		el = questions[getRandom(0, questions.length - 1)].split('~');
+function nextQuestion (count, questions, correct) {
+	try {
+		el = questions[count].split('~');
 		question = el[0];
 		answers = el.slice(1);
 		$('.b-question h3').text(question.replaceAll('"', ''));
-		answers = answers.sort(function(){
-			return Math.random() - 0.5;
-		});
+		answers = answers.sort(() => 0.5 - Math.random());
 		for (i = 0; i < answers.length; i++) {
 			if (answers[i].indexOf('<>') !== -1) {
-				$('.b-answers .li-' + i).addClass('active');
+				$('.b-answers .li-' + i).addClass('active on');
 			}
 			$('.b-answers .li-' + i).text(answers[i].replaceAll('"', '').replace('<>', ''));
 		}
-	});
+	}
+	catch (err) {
+		console.log('End');
+		// alert('Правильных: ' + correct)
+	}
 }
 
 $(document).ready(function () {
@@ -32,10 +31,41 @@ $(document).ready(function () {
 		$('body').css({
 			'display': 'block',
 		})
-		getQuestion();
-		$('body').on('click', '.b-answers li.active', function () {
-			getQuestion();
+
+		$('li').removeClass('active');
+
+		count = 1;
+		correct = 0;
+
+		$.get('/public/js/question.txt', function(data) {
+			questions = data.split('\n');
+			shuffled_questions = questions.sort(() => 0.5 - Math.random());
+			selected_questions = shuffled_questions.slice(0, 26);
+			nextQuestion(count, selected_questions);
+			$('body').on('click', '.b-answers li', function () {
+				if ($(this).hasClass('active')) {
+					correct += 1;
+					$('.code-2').text('Правильных: ' + correct);
+				}
+				$('li').removeClass('active');
+				if (count !== 25) {
+					count += 1;
+					nextQuestion(count, selected_questions, correct);
+				}
+				$('.code-1').text('Вопрос: ' + count);
+			})
+		});
+
+		setInterval(function () {
+			if ($('input').prop('checked')) {
+				$('li.active').addClass('on')
+			}
+			else {
+				$('li.active').removeClass('on')
+			}
 		})
+
+
 	// }
 	// else {
 	// 	window.location.reload();
